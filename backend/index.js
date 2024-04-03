900e6e4d3054e6d19e2922dea8dd76c1e531dd2a
Konsta <konstakauranen@gmail.com>
2024-04-03 17:34:56 +0300

    Add Express static middleware.

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 app.use(morgan('tiny'))
+app.use(express.static('dist'))
 
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
